f4b1f342fe04532d6fd1fb42db5fe38a5b52dc0c
bardock-fedora <bardock-fedora@localhost.localdomain>
2021-05-15 21:25:01 +0530

    Updates

---
+++ precache-manifest.d6b4516ac5da2d16fa156d7d015b7d53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a124591af2a05ca945178a08c48d46ff",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "164ddff9d2be9e14c6e8",
+    "url": "/website/static/css/main.0605835b.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/website/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/website/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164ddff9d2be9e14c6e8",
+    "url": "/website/static/js/main.e333e281.chunk.js"
+  },
+  {
+    "revision": "f7ba02939af9c7e088f6",
+    "url": "/website/static/js/runtime-main.d57c616e.js"
+  },
+  {
+    "revision": "2660ed21ec82c12f69befa3f81a213f1",
+    "url": "/website/static/media/habeeb.2660ed21.jpg"
+  },
+  {
+    "revision": "ddc94b75b2119f5b080dccb9e17264ee",
+    "url": "/website/static/media/resume.ddc94b75.pdf"
+  }
+]);
